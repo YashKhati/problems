e96e9284aefbe5e86543a277d33cac61791349ec
Yash Khati <103344945+YashKhati@users.noreply.github.com>
2022-11-09 20:04:43 +0530

    Update ClimbingStairs.cpp

--- Dynamic Programming/ClimbingStairs.cpp
+++ Dynamic Programming/ClimbingStairs.cpp
@@ -20,3 +20,29 @@ public:
         return NumberOfWays(n-1)+NumberOfWays(n-2);
     }
 };
+
+
+// Memoization Method
+// TC = O(n)
+// SC = O(n)
+
+class Solution {
+public:
+    int climbStairs(int n)
+    {
+        vector<int> dp(n+1,0);
+        return NumberOfWays(n,dp);
+    }
+     int NumberOfWays(int n,vector<int> &dp)
+    {
+        if(n<=2)
+        {
+            return dp[n]=n;
+        }
+        if(dp[n]!=0)
+        {
+            return dp[n];
+        }
+        return dp[n]=NumberOfWays(n-1,dp)+NumberOfWays(n-2,dp);
+    }
+};
