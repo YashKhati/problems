05f7078f9821db8bdbe72b0bf8790c9b749c29ed
Yash Khati <103344945+YashKhati@users.noreply.github.com>
2022-12-27 23:22:04 +0530

    Create RandomizeQuickSort.cpp

---
+++ Sorting/RandomizeQuickSort.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+int partition(int arr[],int l,int r)
+{
+    int x = (rand()%(r-l+1))+l;
+    swap(arr[x],arr[r]);
+    int pivot = arr[x];
+    int i=l-1;
+    for(int j=l;j<= r-1;j++)
+    {
+        if(arr[j]<pivot)
+        {
+            i++;
+            swap(arr[i],arr[j]);
+        }
+    }
+    swap(arr[i+1],arr[r]);
+    return (i+1);
+}
+void QuickSort(int arr[],int l,int r)
+{
+    if(l<r)
+    {
+        int pivot = partition(arr,l,r); 
+        QuickSort (arr,l,pivot-1);
+        QuickSort(arr,pivot+1,r);
+    }
+}
+int main()
+{
+    int n;
+    cout<<"Enter number of elements : ";
+    cin>>n;
+    int arr[n];
+    for(auto &it :arr)
+    {
+        cin>>it;
+    }
+    cout<<"Quick Sort : \n";
+    QuickSort(arr,0,n-1);
+    for(auto &it : arr)
+    {
+        cout<<it<<" ";
+    }
+    cout<<endl;
+}
