b937882114064f90cab41dae31b81fd6299dfb0b
Yash Khati <103344945+YashKhati@users.noreply.github.com>
2023-02-20 21:14:28 +0530

    Create 35_SearchInsertPosition.cpp

---
+++ leetcode/35_SearchInsertPosition.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target)
+    {
+        int low=0;
+        int high = nums.size()-1;
+        while(low<=high)
+        {
+            int mid = low + (high-low)/2;
+            if( target == nums[mid] )
+            {
+                return mid;
+            }
+            else if(nums[mid] > target)
+            {
+                high = mid-1;
+            }
+            else
+            {
+                low = mid+1;
+            }
+        }
+     
+        return low;
+
+    }
+};
