2f43a89daa611ac5c01ea25a3bdaa2eafe50b1c5
Yash Khati <103344945+YashKhati@users.noreply.github.com>
2024-03-03 11:25:48 +0530

    Update MinSwapToSortArray.py

--- Graph/MinSwapToSortArray.py
+++ Graph/MinSwapToSortArray.py
@@ -1,43 +1,29 @@
-nums = [2, 3, 1, 2, 4, 3]
-target = 7
-shortest = float('inf')
-prefix_sums = [0 for num in nums]
-prefix_sums[0] = nums[0]
-for i in range(1, len(nums)):
-    prefix_sums[i] = prefix_sums[i - 1] + nums[i]
+nums = [6, 4, 2, 14, 8, 10, 12, 16]
+
+print("Array : ",nums)
+
+elementPos = []
+for i in range(len(nums)):
+    elementPos.append((i,nums[i]))
+print("Before Sorting element position array : ", elementPos)
 
 
-def binary_search(i, prefix_sums):
+elementPos.sort(key=lambda x: x[1])
+print("After Sorting element position array :  ", elementPos)
 
-    l = i
-    r = len(prefix_sums)
-    # it has to be greater than last index or
-    # if you have one element, you will return back
-    # the wrong element. E.g. [10] target== 20
-    # w/o correction l == r immediately and you return index 0
-    # but that is not correct, by r out of range, you will
-    # return a l that is out of range so you know what happened.
-    while l < r:
-        m = l + (r - l) // 2
-        if prefix_sums[m] - prefix_sums[i] + nums[i] >= target:
-            r = m
-        else:
-            l = m + 1
+vis = [False] * len(nums)
+ans = 0
+for i in range( len(nums)):
+    if vis[i] == True or elementPos[i][0] == i:
+        continue
+    else:
+        cycle_len = 0
+        j = i
+        while vis[j] == False:
+            vis[j] = True
+            cycle_len += 1
+            j = elementPos[j][0]
 
-    return l
+        ans += (cycle_len-1)
 
-
-for i in range(0, len(nums)):
-    j = binary_search(i, prefix_sums)
-    if j < len(prefix_sums):  # you need this because the condition might not be satisfied
-        shortest = min(shortest, j - i + 1)
-
-        # brute force commented out. Notice how since the prefix_sums
-        # is increasing, the brute force break condition becomes the
-        # "find condition" in the binary search. Originally we had:
-        # for j in range(0, len(nums)):
-        #     if prefix_sums[j]-prefix_sums[i] + nums[i] >= target:
-        #         shortest = min(shortest, j-i+1)
-        #         break
-
-print(shortest) if shortest != float('inf') else print("-1")
+print(ans)
